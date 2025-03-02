1ce5d1d93d958f2379d7296c7ee6fb1b9d8810e1
adityakumar1120 <adityaakumar1120@gmail.com>
2025-03-02 21:42:14 +0530

    added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const arr =[
     {name:'shivam', image:'./img/shivam.jpg'},
     {name:'yashshree', image:'./img/yashshree4.jpg'},
-    {name:'patals of flower', image:'https://images.unsplash.com/photo-1643754005104-c607eb6978dc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG5hdXR1cmV8ZW58MHx8MHx8fDA%3D'},
+    {name:'Kajal Didi', image:'./img/kajalDidi.jpeg'},
     {name:'patals of flower', image:'https://images.unsplash.com/photo-1656512893645-225dffaf283f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fG5hdXR1cmV8ZW58MHx8MHx8fDA%3D'},
     {name:'patals of flower', image:'https://images.unsplash.com/photo-1541250562733-2eddcc44e39d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGV0YWxzJTIwb2YlMjByb3NlfGVufDB8fDB8fHww'},
     {name:'patals of flower', image:'https://plus.unsplash.com/premium_photo-1708110921398-1fc68e98eacc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZWxzfGVufDB8fDB8fHww'},
